31589b7b3be3354c3a3f456b44f17fef34dcc3a4
Mohsin Sayed <mohsinsayed1999@gmail.com>
2023-09-08 17:37:19 +0530

    Base Website setup base ReactJS website with React Router

---
+++ src/Screens/ErrorScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ErrorScreen extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Error Screen</h1>
+            </div>
+        );
+    }
+}
+
+export default ErrorScreen;
