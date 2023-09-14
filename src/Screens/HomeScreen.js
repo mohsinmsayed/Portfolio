19c86c55b87561624c3583837c96de8648525e55
Mohsin Sayed <mohsinsayed1999@gmail.com>
2023-09-14 23:40:14 +0530

    About Section

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,12 +3,14 @@ import "../StyleSheets/HomeScreen.css";
 
 // importing sections
 import IntroSection from "../Sections/HomeScreen/IntroSection";
+import AboutSection from "../Sections/HomeScreen/AboutSection";
 
 class HomeScreen extends React.Component {
     render() {
         return (
             <div id="HomeScreen">
                 <IntroSection />
+                <AboutSection />
             </div>
         );
     }
---
+++ src/Sections/HomeScreen/AboutSection.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../StyleSheets/AboutSection.css";
+
+class AboutSection extends React.Component {
+    render() {
+        return (
+            <div id="AboutSection">
+                <div id="TextBox">
+                    <h1>------- about ME -------</h1>
+                    <h2>I am a DEVELOPER / PROGRAMMER</h2>
+                    <h2>
+                        I build accessible, inclusive products and digital
+                        experiences on the web
+                    </h2>
+                    <h2>
+                        I am a Computer Science Graduate with expertise in Web
+                        Development, Cross-Platform App Development, and
+                        ServiceNOW domains
+                    </h2>
+                </div>
+                <h1>------- skills -------</h1>
+                <div id="SkillSection">
+                    <div>
+                        <h1>WEB DEVELOPMENT</h1>
+                    </div>
+                    <div>
+                        <h1>CROSS-PLATFORM APP DEVELOPMENT</h1>
+                    </div>
+                    <div>
+                        <h1>ServiceNOW&reg; DEVELOPER</h1>
+                    </div>
+                </div>
+                <Link to="/about" id="AboutButton">
+                    Explore About ME
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default AboutSection;
