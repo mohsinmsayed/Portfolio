0841754daaea87074d42701f53b5cc6e2a32ba09
Mohsin Sayed <mohsinsayed1999@gmail.com>
2023-09-15 12:36:06 +0530

    Project Section

---
+++ src/Sections/HomeScreen/ProjectSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../StyleSheets/ProjectSection.css";
+
+class ProjectSection extends React.Component {
+    render() {
+        return (
+            <div id="ProjectSection">
+                <h1>------- projects/endeavours -------</h1>
+                <h2>
+                    I've created several accessible, inclusive products and
+                    digital experiences on the web
+                </h2>
+                <Link to="/projects" id="ProjectButton">
+                    <img
+                        src="assets/icons/project-w.png"
+                        alt="home-icon"
+                        width="25"
+                    />
+                    Explore Projects
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default ProjectSection;
