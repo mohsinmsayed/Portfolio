c97c5796041671f96f48a048242d2695e488451d
Mohsin Sayed <mohsinsayed1999@gmail.com>
2023-09-18 03:25:03 +0530

    Education Section

--- src/Screens/AboutScreen.js
+++ src/Screens/AboutScreen.js
@@ -3,6 +3,7 @@ import "../StyleSheets/AboutScreen.css";
 
 // importing sections
 import IntroductionSection from "../Sections/AboutScreen/IntroductionSection";
+import EducationSection from "../Sections/AboutScreen/EducationSection";
 import ContactScreen from "./ContactScreen";
 
 class AboutScreen extends React.Component {
@@ -10,6 +11,7 @@ class AboutScreen extends React.Component {
         return (
             <div id="AboutScreen">
                 <IntroductionSection />
+                <EducationSection />
                 <ContactScreen />
             </div>
         );
